7e1ea031f4dc81186169700ccbc6a99c308bd620
maleksal <malek.salem.14@gmail.com>
2021-06-22 16:51:08 +0100

    add task 3

---
+++ 0x0C-Javascript_advanced/3-classrooms.js
@@ -0,0 +1,15 @@
+function createClassRoom(numberOfStudents) {
+    
+    function studentSeat(seat) {
+        return function(){ return seat };
+    }
+	
+    let students = [];
+	
+    for (let i = 0; i < numberOfStudents; i++) {
+		students.push(studentSeat(i + 1));
+	}
+	return students;
+}
+
+let classRoom = createClassRoom(10);
